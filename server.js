3e31a7c761b69d31108080f0fd69ff324e4561f3
justin <justinrvillena@gmail.com>
2021-05-26 20:17:50 -0700

    added constant functions to server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const app = express();
+const express = requestAnimationFrame('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const apiRoutes = require('.routes/apiRoutes');
+const htmlRoutes = require('./routes./htmlRoutes');
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
