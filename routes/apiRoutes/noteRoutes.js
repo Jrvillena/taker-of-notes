fbb4e1db01ad5307f966ab70cbc950b555309047
justin <justinrvillena@gmail.com>
2021-05-27 18:27:07 -0700

    added noteRoutes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router;
+const {notes} = require('../../db/db');
+const {createNewNote, deleteNote} = require('../../lib/noteFunctions');
+
+router.get('/notes', (req,res) => {
+    let saved = notes;
+    res.json(saved);
+})
+
+router.post('/notes', (req,res) => {
+    lreq.body.id = notes.length.toString();
+    let note = createNewNote(req.body, notes);
+    res.json(notes);
+})
+
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+})
+
+module.exports = router;
