b0845ffb30508e9617c366e8dfc5713d74bb95b0
justin <justinrvillena@gmail.com>
2021-05-28 18:05:04 -0700

    noteFunctions js file completed

--- Develop/lib/noteFunctions.js
+++ Develop/lib/noteFunctions.js
@@ -15,3 +15,24 @@ function createNewNote(body, notesArray) {
         return note;
 
 }
+
+function deleteNote(notesArray, id) {
+    let deleteId = parseInt(id);
+    notesArray.splice(deleteId, 1);
+
+    for (let i = deleteId; i < notesArray.length; i++) {
+        notesArray[i].id = i.toString();
+    }
+
+    fs.writeFile(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify({
+            notes: notesArray
+        }, null, 2)
+    )
+}
+
+module.exports = {
+    createNewNote,
+    deleteNote
+};
